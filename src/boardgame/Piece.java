e3e8443a25e91534982183f9cfc872c7e55389a0
Pedro Amorim <pedrok.amorim07@gmail.com>
2026-02-02 15:18:16 -0300

    Chess layer and printing the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,4 +2,16 @@ package boardgame;
 
 public class Piece {
 
+	protected Position postion;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	
 }
