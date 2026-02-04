17bac04788994964dd57e5d2a7d75ba082caefb9
Pedro Amorim <pedrok.amorim07@gmail.com>
2026-02-04 17:26:41 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,27 +18,27 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while (true) { 
+		while (!chessMatch.getCheckMate()) { 
 			try {
-			UI.clearScreen();
-			UI.printMatch(chessMatch, captured);
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.ReadChessPosition(sc);
-			
-			boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-			UI.clearScreen();
-			UI.printBoard(chessMatch.getPieces(), possibleMoves);
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.ReadChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			
-			if (capturedPiece != null) {
-				captured.add(capturedPiece);
-			}
-		}	
+				UI.clearScreen();
+				UI.printMatch(chessMatch, captured);
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.ReadChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.ReadChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
+			}	
 			
 		catch (ChessException e) {
 			System.out.println(e.getMessage());
@@ -50,6 +50,9 @@ public class Program {
 			sc.nextLine();
 		}
 	}
-}
+	
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
+	}
 
 }
